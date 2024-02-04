09a86b7d94c1af2e212c55e96cced99c92d2d77e
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-02-04 18:56:01 +0200

    add  fadding animation to splash view

--- lib/fetures/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/fetures/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? faddingAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    faddingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!); 
+
+    animationController?.repeat(reverse: true);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +30,23 @@ class SplashViewBody extends StatelessWidget {
         SizedBox(
           height: MediaQuery.of(context).size.height * .4,
         ),
-        const Center(
-          child: Text(
-            'Fruit Market',
-            style: TextStyle(
-                fontSize: 55, fontWeight: FontWeight.w700, color: Colors.white),
+        Center(
+          child: FadeTransition(
+            opacity: faddingAnimation!,
+            child: const Text(
+              'Fruit Market',
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white),
+            ),
           ),
         ),
         Expanded(
-            child: Image.asset(
-          'assets/splash_view_image.png',
-        ))
+          child: Image.asset(
+            'assets/splash_view_image.png',
+          ),
+        )
       ],
     );
   }
